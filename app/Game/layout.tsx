c5a90c7aaa56c6252406a2e348d1f1c9f23f9b7e
Siddardhmustplay <siddumustplaygames@gmail.com>
2025-08-20 09:51:34 +0530

    Update layout.tsx

--- app/Game/layout.tsx
+++ app/Game/layout.tsx
@@ -5,14 +5,14 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <html>
       <head />
       <body className="text-white font-sans">
-        <nav className="w-full px-6 py-4 text-center text-xl font-bold text-[#FF6C00] shadow-md border-b border-[#7D4AFF] space-x-6">
-          <Link href="/emoji-frequency" className="hover:underline">🧠 Color Frequency</Link>
-          <Link href="/emoji-duplicator" className="hover:underline">🌀 Color Duplicator</Link>
-          <Link href="/scroll-catcher" className="hover:underline">🎯 Color Catcher</Link>
-          <Link href="/Color-memory" className="hover:underline">🎯 Color Catcher</Link>
-          <Link href="/Color-rush" className="hover:underline">🎯 Color Rush</Link>
-          <Link href="/Color-command" className="hover:underline">🎯 Color command</Link>
-          <Link href="/fading-cleaner" className="hover:underline">Fading cleaner</Link>
+        <nav className="w-full px-6 py-4 text-center text-xl font-bold text-[#FF6C00] shadow-md space-x-6">
+          <Link href="/emoji-frequency" className="hover:underline">Color Frequency</Link>
+          <Link href="/emoji-duplicator" className="hover:underline">Color Duplicator</Link>
+          <Link href="/scroll-catcher" className="hover:underline">Color Catcher</Link>
+          <Link href="/Color-memory" className="hover:underline">Color Catcher</Link>
+          <Link href="/Color-rush" className="hover:underline">Color Rush</Link>
+          <Link href="/Color-command" className="hover:underline">Color Command</Link>
+          <Link href="/fading-cleaner" className="hover:underline">Color Cleaner</Link>
           <Link href="/color-count" className="hover:underline">Color-Count</Link>
           <Link href="/color-arithmetic" className="hover:underline">Color Arithmetic</Link>
         </nav>
