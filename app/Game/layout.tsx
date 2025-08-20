ee8d541ec3d7b820a395883df6b7f44ef1560ca7
Siddardhmustplay <siddumustplaygames@gmail.com>
2025-08-20 10:20:47 +0530

    Update layout.tsx

--- app/Game/layout.tsx
+++ app/Game/layout.tsx
@@ -6,15 +6,15 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <head />
       <body className="text-black font-sans">
         <nav className="w-full px-6 py-4 text-center text-xl font-bold shadow-md space-x-6">
-          <Link href="/emoji-frequency" className="hover:underline">Color Frequency</Link>
-          <Link href="/emoji-duplicator" className="hover:underline">Color Duplicator</Link>
-          <Link href="/scroll-catcher" className="hover:underline">Color Catcher</Link>
-          <Link href="/Color-memory" className="hover:underline">Color Matcher</Link>
-          <Link href="/Color-rush" className="hover:underline">Color Rush</Link>
-          <Link href="/Color-command" className="hover:underline">Color Command</Link>
-          <Link href="/fading-cleaner" className="hover:underline">Color Cleaner</Link>
-          <Link href="/color-count" className="hover:underline">Color Count</Link>
-          <Link href="/color-arithmetic" className="hover:underline">Color Arithmetic</Link>
+          <Link href="/Game/emoji-frequency" className="hover:underline">Color Frequency</Link>
+          <Link href="/Game/emoji-duplicator" className="hover:underline">Color Duplicator</Link>
+          <Link href="/Game/scroll-catcher" className="hover:underline">Color Catcher</Link>
+          <Link href="/Game/Color-memory" className="hover:underline">Color Matcher</Link>
+          <Link href="/Game/Color-rush" className="hover:underline">Color Rush</Link>
+          <Link href="/Game/Color-command" className="hover:underline">Color Command</Link>
+          <Link href="/Game/fading-cleaner" className="hover:underline">Color Cleaner</Link>
+          <Link href="/Game/color-count" className="hover:underline">Color Count</Link>
+          <Link href="/Game/color-arithmetic" className="hover:underline">Color Arithmetic</Link>
         </nav>
         {children}
       </body>
